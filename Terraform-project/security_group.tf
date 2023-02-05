43771769330a65c67c4beac0b32b2197e356edcd
Rasheedat atinuke jamiu <56790407+RashRAJ@users.noreply.github.com>
2023-02-05 12:09:46 +0100

    Create security_group.tf

---
+++ Terraform-project/security_group.tf
@@ -0,0 +1,86 @@
+#######################################################################
+##                     Load balancer sequrity group                  ##
+#######################################################################
+
+
+resource "aws_security_group" "alb" {
+  name        = "terraform_alb_security_group"
+  description = "Terraform load balancer security group"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic.
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-example-alb-security-group"
+  }
+}
+
+#######################################################################
+##                     Instance sequrity group                       ##
+#######################################################################
+
+resource "aws_security_group" "sg" {
+
+  name        = "instance-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description     = "HTTP"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
+  }
+  ingress {
+    description     = "HTTPS"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
+  }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "AWS security group dynamic block"
+  }
+
+}
+
+
